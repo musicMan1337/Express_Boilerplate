02a92011b873f99a07db2bbd95c96dfed44e1c62
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-09-29 08:05:26 -0700

    SAVEPOINT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const logger = require('../../src/libs/logger');
-const { PORT } = require('../../src/config/envConfig');
+const logger = require('../src/libs/logger');
+const { PORT } = require('../src/config/envConfig');
 
 app.listen(PORT, () => {
   logger.http(`Server listening at http://localhost:${PORT}`);
